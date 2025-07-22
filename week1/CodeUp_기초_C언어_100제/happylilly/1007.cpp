e2ea9a64fae6029bc2e9174ec3c807bf70e73ad3
Happylilly <moon05lilly@gmail.com>
2025-07-22 16:17:31 +0900

    C++ 기초 문법 주차 삭제

--- week1/CodeUp_기초_C언어_100제/happylilly/1007.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using std::cout;
-
-int main() {
-    cout << "\"C:\\Download\\hello.cpp\"";
-    return 0;
-}
--- week1/CodeUp_기초_C언어_100제/happylilly/1008.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using std::cout;
-
-int main() {
-    cout << "\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518";
-    return 0;
-}
