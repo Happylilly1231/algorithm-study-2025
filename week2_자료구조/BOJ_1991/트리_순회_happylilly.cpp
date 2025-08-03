e307bf9e2da9951b8fe8ada11d7d28ea106e07ee
Happylilly <moon05lilly@gmail.com>
2025-08-03 21:06:56 +0900

    트리 순회 / 실버 1 / 1시간 20분 / O

---
+++ week2_자료구조/BOJ_1991/트리_순회_happylilly.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Node
+{
+    char data;
+    Node *left;
+    Node *right;
+
+    Node(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+Node *findNode(Node *root, char value);
+void insertNode(Node *root, char p, char lc, char rc);
+void preorder(Node *root);
+void inorder(Node *root);
+void postorder(Node *root);
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    char p, lc, rc;
+
+    cin >> N;
+
+    Node *root = new Node('A');
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> p >> lc >> rc;
+        insertNode(root, p, lc, rc);
+    }
+
+    preorder(root); // 전위 순회
+    cout << '\n';
+
+    inorder(root); // 중위 순회
+    cout << '\n';
+
+    postorder(root); // 후위 순회
+
+    return 0;
+}
+
+// 같은 값을 가진 노드 찾기
+Node *findNode(Node *root, char value)
+{
+    if (root == nullptr)
+        return nullptr;
+
+    if (root->data == value)
+    {
+        return root;
+    }
+
+    Node *node = findNode(root->left, value);
+
+    if (node == nullptr)
+        node = findNode(root->right, value);
+
+    return node;
+}
+
+// 노드 삽입
+void insertNode(Node *root, char p, char lc, char rc)
+{
+    Node *node = findNode(root, p);
+
+    node->left = new Node(lc);
+    node->right = new Node(rc);
+}
+
+// 전위 순회 : 루트 -> 왼쪽 자식 -> 오른쪽 자식
+void preorder(Node *root)
+{
+    if (root->data == '.')
+        return;
+
+    cout << root->data;
+    preorder(root->left);  // 왼쪽 자식
+    preorder(root->right); // 오른쪽 자식
+}
+
+// 중위 순회 : 왼쪽 자식 -> 루트 -> 오른쪽 자식
+void inorder(Node *root)
+{
+    if (root->data == '.')
+        return;
+
+    inorder(root->left); // 왼쪽 자식
+    cout << root->data;
+    inorder(root->right); // 오른쪽 자식
+}
+
+// 후위 순회 : 왼쪽 자식 -> 오른쪽 자식 -> 루트
+void postorder(Node *root)
+{
+    if (root->data == '.')
+        return;
+
+    postorder(root->left);  // 왼쪽 자식
+    postorder(root->right); // 오른쪽 자식
+    cout << root->data;
+}
