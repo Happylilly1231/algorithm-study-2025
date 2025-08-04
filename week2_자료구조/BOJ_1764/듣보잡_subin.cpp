a3f3846e5987007e8bc5d9e790f95639f68b94a7
정수빈 <jungsubin83@naver.com>
2025-08-04 23:30:55 +0900

    듣보잡 / 실버 4 / 10분 / O

---
+++ week2_자료구조/BOJ_1764/듣보잡_subin.cpp
@@ -0,0 +1,48 @@
+/*
+    알고리즘 : 자료구조 - 맵
+    시간복잡도 : O(nlogN)
+    아이디어 : 이름을 key로 하는 map을 생성하여 등장 횟수를 저장
+*/
+
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n, m, cnt = 0;
+    string name;
+    map<string, int> name_cnt;
+
+    cin >> n >> m;
+
+    while (n--)
+    {
+        cin >> name;
+        name_cnt[name]++;
+    }
+    while (m--)
+    {
+        cin >> name;
+        name_cnt[name]++;
+        if (name_cnt[name] == 2)
+            cnt++;
+    }
+
+    cout << cnt << '\n';
+
+    for (auto iter : name_cnt)
+    {
+        if (iter.second == 2)
+        {
+            cout << iter.first << '\n';
+        }
+    }
+
+    return 0;
+}
