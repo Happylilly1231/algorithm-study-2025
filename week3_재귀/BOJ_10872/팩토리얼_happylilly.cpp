b5b0bd9aaa6c00d93c49071ca5627308e5fde68e
Happylilly <moon05lilly@gmail.com>
2025-08-05 16:22:38 +0900

    (수정) 팩토리얼 / 브론즈 3 / 8분 / O
    수정 부분 : 반환 부분 간략화

--- week3_재귀/BOJ_10872/팩토리얼_happylilly.cpp
+++ week3_재귀/BOJ_10872/팩토리얼_happylilly.cpp
@@ -15,9 +15,7 @@ int factorial(int N)
     if (N == 0 || N == 1)
         return 1;
 
-    int r = 1;
-    r = factorial(N - 1) * N;
-    return r;
+    return factorial(N - 1) * N;
 }
 
 int main()
